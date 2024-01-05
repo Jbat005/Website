4626cf718015b985a59f0525fd2994dd40694a89
Jean <jbatist005@citymail.cuny.edu>
2024-01-05 18:05:30 -0500

    Edit text

--- script.js
+++ script.js
@@ -9,18 +9,21 @@ document.getElementById('menu-toggle').addEventListener('click', function() {
 const projects = [
     { 
         name: 'Pizza Restaurant Analysis - Full website with interactive dashboard', 
-        description: `My first data analysis project, I was given a dataset of a pizza restaurant.
-        The task was to prepare the data using SQL. Python was the language that I used for the analysis,
-        with the goal to create a report detailing the opportunities for the restaurant. I decided to make a 
-        web app, just as a learning experiment. Clicking on the image will take you to the web app.`, 
+        description: `I was given an operational database of a pizza restaurant, and my task was to provide
+        opportunities for success for the restaurant through a report after first preparing the data with MySQL.
+        I cleaned up the data with Python and Pandas and used the data available to me to visualize the most critical
+        takeaways from the year's worth of data. I then added interactivity and pushed it to a web app, 
+        allowing the user to explore the data themselves.`, 
         imageUrl: 'pizzaproject.jpg', 
         link: 'https://doc-pastor-remote-l069-2273e74aa948.herokuapp.com' 
     },
     { 
         name: 'Berry Cox Price Momentum Baskets & Monte Carlo Efficient Frontier - Quantitative Finance Project', 
-        description: `Created a trading strategy based on price momentum factors, back tested for performance against the S&P 500,
-        it calculates the momentum factors using pandas and NumPy library 
-        to to curate long and short stock baskets. Then performs comparative performance analysis against S&P 500, highlighting strategy efficacy.
+        description: `Implements a trading strategy based on Berry Cox Alpha Generators, specifically
+        the price momentum factors. These factors can predict what an asset's expected growth will be.
+        It grabs a basket of the asset's with the highest factor-based score, and another basket with the lowest.
+        Once it curated long and short baskets, it performs comparative performance analysis against S&P 500,
+        highlighting strategy efficacy. Then outputting the most optimal weights of each asset.
         Clicking on the image will take you to the GitHub page.`,
         imageUrl: 'berrycox1.png', // Only one image
         link: 'https://github.com/CCNY-Analytics-and-Quant/Quantative-Finance-Repo/blob/main/Berry-Cox-Baskets/final_project.ipynb'
