eec3053c84f239efd8f1f616669235ccbfdcab69
Jean <jbatist005@citymail.cuny.edu>
2024-01-14 21:57:28 -0500

    cleaner ui

--- script.js
+++ script.js
@@ -9,22 +9,16 @@ document.getElementById('menu-toggle').addEventListener('click', function() {
 const projects = [
     { 
         name: 'Pizza Restaurant Analysis - Full website with interactive dashboard', 
-        description: `I was given an operational database of a pizza restaurant, and my task was to provide
-        opportunities for success for the restaurant through a report after first preparing the data with MySQL.
-        I cleaned up the data with Python and Pandas and used the data available to me to visualize the most critical
-        takeaways from the year's worth of data. I then added interactivity and pushed it to a web app, 
-        allowing the user to explore the data themselves.`, 
+        description: `This SQL database contains pizza restaurant data. I created a report of the best opportunities for success, and visualized the most critical
+        takeaways from the year's worth of data. Click on the image to explore the data yourself.`, 
         imageUrl: 'pizzaproject.jpg', 
         link: 'https://doc-pastor-remote-l069-2273e74aa948.herokuapp.com' 
     },
     { 
         name: 'Berry Cox Price Momentum Baskets & Monte Carlo Efficient Frontier - Quantitative Finance', 
-        description: `Implements a trading strategy based on Berry Cox Alpha Generators, specifically
-        the price momentum factors. These factors can predict what an asset's expected growth will be.
-        It grabs a basket of the asset's with the highest factor-based score, and another basket with the lowest.
-        Once it curated long and short baskets, it performs comparative performance analysis against S&P 500,
-        highlighting strategy efficacy. Then outputting the most optimal weights of each asset.
-        Clicking on the image will take you to the GitHub page.`,
+        description: `Implements price momentum factors trading strategy, that predict asset's expected growth.
+        Curates long and short baskets, highlighting strategy efficacy over long run. Lastly, optimizes portfolio weights using monte carlo simulation.
+        Click on the image to go to the GitHub page.`,
         imageUrl: 'berrycox1.png', // Only one image
         link: 'https://github.com/CCNY-Analytics-and-Quant/Quantative-Finance-Repo/blob/main/Berry-Cox-Baskets/final_project.ipynb'
     }
@@ -38,16 +32,21 @@ function displayProjects() {
         const projectElement = document.createElement('div');
         projectElement.classList.add('project');
 
-        let projectContent = `<h3>${project.name}</h3><p>${project.description}</p>`;
-
+        const projectText = document.createElement('div');
+        projectText.classList.add('project-text');
+        projectText.innerHTML = `<h3>${project.name}</h3><p>${project.description}</p>`;
+        
+        const projectImg = document.createElement('div');
+        projectImg.classList.add('project-img');
         if (project.imageUrl) {
-            // Add target="_blank" to open in a new tab
-            projectContent += `<a href="${project.link}" target="_blank"><img src="${project.imageUrl}" alt="${project.name}"></a>`;
+            projectImg.innerHTML = `<a href="${project.link}" target="_blank"><img src="${project.imageUrl}" alt="${project.name}"></a>`;
         }
 
-        projectElement.innerHTML = projectContent;
+        projectElement.appendChild(projectText);
+        projectElement.appendChild(projectImg);
         projectsContainer.appendChild(projectElement);
     });
 }
+
 
 document.addEventListener('DOMContentLoaded', displayProjects);
